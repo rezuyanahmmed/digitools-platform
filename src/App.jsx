d9e003d30b394ab99bdf060b5186e0ba4a379deb
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-21 00:57:35 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
@@ -9,17 +7,45 @@ function App() {
 
   return (
     <>
-    <h2 className='text-red-500 font-bold'>
-      hello
-    </h2>
-    <p className='text-blue-500'>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, veniam?
-    </p>
-    
-      
+      {/* Navbar */}
+      <div className="navbar bg-base-100  w-[80%] mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><a>Products</a></li>
+              <li><a>Features</a></li>
+              <li><a>Pricing</a></li>
+              <li><a>Testimonials</a></li>
+              <li><a>FAQ</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-3xl text-[#4F39F6]">DigiTools</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Products</a></li>
+            <li><a>Features</a></li>
+            <li><a>Pricing</a></li>
+            <li><a>Testimonials</a></li>
+            <li><a>FAQ</a></li>
 
-      
-     
+          </ul>
+        </div>
+        <div className="navbar-end gap-3">
+          <button className=''>Login</button>
+          <button className='btn bg-[#4F39F6] text-white rounded-3xl'>Get Started</button>
+        </div>
+      </div>
+
+
+
+
+
     </>
   )
 }
