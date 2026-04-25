ae577b266e8b8aa924ab1b61546eb8a2ba5b768a
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-25 23:56:41 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ import Cart from "./Components/navbar/Cart"; // Premium এর বদলে Cart
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import bannerPhoto from "./assets/banner.png";
-import userImg from "./assets/user.png";
-import packageImg from "./assets/package.png";
-import rocketImg from "./assets/rocket.png";
+
+import Started from "./Components/navbar/Started";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
@@ -44,38 +43,7 @@ function App() {
         onCheckout={handleCheckout}></Cart>
 
       {/* Get started section */}
-      <div className="mt-7">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-2xl">Get Started in 3 Steps</h1>
-          <p>Start using premium digital tools in minutes, not hours.</p>
-        </div>
-        {/* 1st */}
-        <div className="flex w-[80%] mx-auto space-x-3 mt-7">
-          <div className="card w-96 bg-base-100 card-lg shadow-sm">
-            <div className="card-body flex justify-center items-center">
-              <img className="h-12 w-12 bg-purple-200 p-2 rounded-full" src={userImg} alt="" />
-              <h4 className="font-bold ">Create Account</h4>
-              <p className="text-gray-600">Sign up for free in seconds. No credit card required to get started.</p>
-            </div>
-          </div>
-          {/* 2nd */}
-          <div className="card w-96 bg-base-100 card-lg shadow-sm">
-            <div className="card-body flex justify-center items-center">
-              <img className="h-12 w-12 bg-purple-200 p-2 rounded-full" src={packageImg} alt="" />
-              <h4 className="font-bold ">Choose Products</h4>
-              <p className="text-gray-600">Browse our catalog and select the tools that fit your needs.</p>
-            </div>
-          </div>
-          {/* 3rd */}
-          <div className="card w-96 bg-base-100 card-lg shadow-sm">
-            <div className="card-body flex justify-center items-center">
-              <img className="h-12 w-12 bg-purple-200 p-2 rounded-full" src={rocketImg} alt="" />
-              <h4 className="font-bold ">Start Creating</h4>
-              <p className="text-gray-600">Download and start using your premium tools immediately.</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Started></Started>
 
     </>
   );
---
+++ src/Components/navbar/Started.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import userImg from "./assets/user.png";
+import packageImg from "./assets/package.png";
+import rocketImg from "./assets/rocket.png";
+
+const Started = () => {
+  return (
+    <div className="mt-7">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="font-bold text-2xl">Get Started in 3 Steps</h1>
+        <p>Start using premium digital tools in minutes, not hours.</p>
+      </div>
+      {/* 1st */}
+      <div className="flex w-[80%] mx-auto space-x-3 mt-7">
+        <div className="card w-96 bg-base-100 card-lg shadow-sm">
+          <div className="card-body flex justify-center items-center">
+            <img className="h-12 w-12 bg-purple-200 p-2 rounded-full" src={userImg} alt="" />
+            <h4 className="font-bold ">Create Account</h4>
+            <p className="text-gray-600">Sign up for free in seconds. No credit card required to get started.</p>
+          </div>
+        </div>
+        {/* 2nd */}
+        <div className="card w-96 bg-base-100 card-lg shadow-sm">
+          <div className="card-body flex justify-center items-center">
+            <img className="h-12 w-12 bg-purple-200 p-2 rounded-full" src={packageImg} alt="" />
+            <h4 className="font-bold ">Choose Products</h4>
+            <p className="text-gray-600">Browse our catalog and select the tools that fit your needs.</p>
+          </div>
+        </div>
+        {/* 3rd */}
+        <div className="card w-96 bg-base-100 card-lg shadow-sm">
+          <div className="card-body flex justify-center items-center">
+            <img className="h-12 w-12 bg-purple-200 p-2 rounded-full" src={rocketImg} alt="" />
+            <h4 className="font-bold ">Start Creating</h4>
+            <p className="text-gray-600">Download and start using your premium tools immediately.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Started;
