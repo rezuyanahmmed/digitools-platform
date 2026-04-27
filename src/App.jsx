5a0165071d8dce24e9c224d3e21b0f6d40946bc7
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-27 20:22:12 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ import Started from "./Components/navbar/Started";
 import Pricing from "./Components/navbar/Pricing";
 import Ready from "./Components/navbar/Ready";
 
+import { FiInstagram } from "react-icons/fi";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+
+
+
 function App() {
   const [cartItems, setCartItems] = useState([]);
 
@@ -49,7 +56,7 @@ function App() {
       <Ready></Ready>
 
       {/* Footer */}
-      <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+      <footer className="footer sm:footer-horizontal bg-[#101727] text-neutral-content p-10">
         <div className="w-[300px]">
           <h1 className="text-3xl text-white">DigiTools</h1>
           <p className="text-gray-400">Premium digital tools for creators, professionals, and businesses. Work smarter with our suite of powerful tools.</p>
@@ -74,8 +81,28 @@ function App() {
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
+        <div>
+          <h2>Social Links</h2>
+          <div className="flex gap-4">
+            <a className="bg-white p-2 rounded-full text-black" href=""><FaFacebookSquare /></a>
+            <a className="bg-white p-2 rounded-full text-black" href=""><FiInstagram /></a>
+            <a className="bg-white p-2 rounded-full text-black" href=""><FaXTwitter /></a>
+          </div>          
+        </div>     
+        
       </footer>
+      
 
+      <div className="flex justify-between bg-[#101727] h-[100px] pl-10">
+      
+        <p className="text-white">© 2026 Digitools. All rights reserved.</p>
+        <div className="text-white pr-10">
+          <a href="">Praivacy Policy</a>
+          <a href="">Terms of Service</a>
+          <a href="">Cookis</a>
+        </div>
+      </div>
+     
 
     </>
   );
