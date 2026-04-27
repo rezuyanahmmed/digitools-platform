ca3805cfb18baadad222194bf1aa22c177a734f2
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-27 19:53:48 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import bannerPhoto from "./assets/banner.png";
 
 import Started from "./Components/navbar/Started";
 import Pricing from "./Components/navbar/Pricing";
+import Ready from "./Components/navbar/Ready";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
@@ -45,23 +46,35 @@ function App() {
 
       <Started></Started>
       <Pricing></Pricing>
+      <Ready></Ready>
 
       {/* Footer */}
-      <footer className="h-[300px] mt-20 bg-gradient-to-r from-[#642ef7] to-purple-600 footer footer-horizontal footer-center bg-primary text-primary-content p-10">
-        <div>
-          <h1 className="font-bold text-2xl text-white">Ready to Transform Your Workflow?</h1>
-          <p>Join thousands of professionals who are already using Digitools to work smarter.
-            Start your free trial today.</p>
+      <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+        <div className="w-[300px]">
+          <h1 className="text-3xl text-white">DigiTools</h1>
+          <p className="text-gray-400">Premium digital tools for creators, professionals, and businesses. Work smarter with our suite of powerful tools.</p>
         </div>
         <nav>
-          <div className="grid grid-flow-col gap-4 -mt-14">
-            <button class="btn bg-white rounded-full">Explore Products</button>
-            <button className="btn btn-outline btn-primary rounded-full text-white border-white">View Pricing</button> 
-
-          </div>
-          <p className="text-white mt-2">14-day free trial • No credit card required • Cancel anytime</p>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
         </nav>
-      </footer >
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
 
 
     </>
