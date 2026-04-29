159e391bc67833361da41d254729e922b3f6ee3d
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-04-29 23:40:14 +0600

    first commit

--- src/Components/navbar/Ready.jsx
+++ src/Components/navbar/Ready.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Ready = () => {
   return (
     <footer className="h-[300px] mt-20 bg-gradient-to-r from-[#642ef7] to-purple-600 footer footer-horizontal footer-center bg-primary text-primary-content p-10">
-      {/* <div>
+      <div>
         <h1 className="font-bold text-2xl text-white">Ready to Transform Your Workflow?</h1>
         <p>Join thousands of professionals who are already using Digitools to work smarter.
           Start your free trial today.</p>
-      </div> */}
+      </div>
       <nav>
         <div className="grid grid-flow-col gap-4 -mt-14">
           <button className="btn bg-white rounded-full">Explore Products</button>
